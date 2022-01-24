b47419e2f337924399a4b0fa243cc03c6ec08839
JJSolvang <jacobsolvang@gmail.com>
2022-01-23 17:31:32 -0800

    Added JS program and updated support in HTML to display the output of the show button.

--- main.js
+++ main.js
@@ -1,31 +1,67 @@
-let movieName = [];
-let movieRating = [];
-let movieYear = [];
+let movies = [];
 
 document.getElementById("movieInfo").addEventListener('click', function () {
 
     if (!(document.getElementById("movieTitle").value == "")) {
-
-        alert("food0");
+        
+        console.log("Title: Passed");
 
         if (!(document.getElementById("movieRating").value == "")) { 
 
-            if (Number.isInteger(parseInt(document.getElementById("movieRating").value)) === true) {
-                alert("food1");
+            if (Number.isInteger(parseInt(document.getElementById("movieRating").value)) === true && parseInt(document.getElementById("movieRating").value) > 0 && parseInt(document.getElementById("movieRating").value) < 6) {
+                console.log("Rating: Passed");
 
                 if (!(document.getElementById("movieYear").value == "")) { 
 
-                    if (Number.isInteger(parseInt(document.getElementById("movieYear").value)) === true) {
-                        alert("food2");
+                    if (Number.isInteger(parseInt(document.getElementById("movieYear").value)) === true && parseInt(document.getElementById("movieYear").value) > 0) {
+                        
+                        console.log("Year: Passed");
+
+                        if (movies.length < 9) {
+
+                            movies[movies.length] = { name : document.getElementById("movieTitle").value, rating : document.getElementById("movieRating").value, year : document.getElementById("movieYear").value };
+
+                            console.log(movies[movies.length - 1]);
+                        } else {
+
+                            alert("You have reached your maximum allowance of movies. Press F5 or refresh to start again.");
+                        }
+                    } else {
+
+                        console.log("Year: Failed");
+                        alert("Data incorrect. Movie could not be saved (Must be a positive integer).");
                     }
                 } else {
-                    alert("Data incorrect. Movie could not be saved.");
+
+                    console.log("Year: Failed");
+                    alert("Data incorrect. Movie could not be saved (Missing Year).");
                 }
+            } else {
+
+                console.log("Rating: Failed");
+                alert("Data incorrect. Must be a rating 1 (worst) - 5 (best).");
             }
         } else {
-            alert("Data incorrect. Movie could not be saved.");
+
+            console.log("Rating: Failed");
+            alert("Data incorrect. Movie could not be saved (Missing Rating).");
         }
     } else {
-        alert("Data incorrect. Movie could not be saved.");
+
+        console.log("Title: Failed");
+        alert("Data incorrect. Movie could not be saved (Missing Title).");
+    }
+});
+
+document.getElementById("movieShow").addEventListener('click', function () {
+
+    movies.sort((a, b) => {
+        return a.rating - b.rating;
+    });
+
+    console.log(movies);
+
+    for (let i = 0; i < movies.length; i++) {
+        document.getElementById("displayMovies").innerHTML = document.getElementById("displayMovies").innerHTML + "<br/>" + (i + 1) + ") " + movies[i].name + ": " + movies[i].rating + "/5 - Released: " + movies[i].year;
     }
 });
